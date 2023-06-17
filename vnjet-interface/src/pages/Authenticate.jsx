6d04f4c388a9441a9cb8b0c3ebe4e392962a015d
20120095 <hungngojan25@gmail.com>
2023-06-17 16:22:51 +0700

    clean authenticate

--- vnjet-interface/src/pages/Authenticate.jsx
+++ vnjet-interface/src/pages/Authenticate.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import LogInForm from "../components/LogInForm";
 import SignUpForm from "../components/SignUpForm";
-import { useGlobal } from "../context/context";
 
 function Authenticate({ submit, entryType }) {
-  const { authenticate } = useGlobal();
-  console.log(authenticate.selectState());
-
-  React.useEffect(() => {
-    authenticate.dispatchLoginStart();
-  }, []);
-
   return (
     <div className="d-flex justify-content-center">
       {entryType == 0 ? (
